551a8e6e86f86adbcb3a92c23003f1913b2312b7
filmo.dev <filmorealty.dev@gmail.com>
2024-03-16 17:58:36 +0100

    made correction...

--- src/pages/dashboard/Properties/Index.jsx
+++ src/pages/dashboard/Properties/Index.jsx
@@ -132,7 +132,7 @@ const Index = () => {
     
                                     </div>
                                     <div className="flex py-4 px-5 flex-col ">
-                                        <Link to='admin/dashboard/listing' className="md:text-sm xs:text-xs px-3 text-gray-500 rounded-3xl px-3 py-2 border-[1px] border-gray-700 w-fit">
+                                        <Link to='/admin/dashboard/listing' className="md:text-sm xs:text-xs px-3 text-gray-500 rounded-3xl px-3 py-2 border-[1px] border-gray-700 w-fit">
                                             View All Listing
                                         </Link>
     
