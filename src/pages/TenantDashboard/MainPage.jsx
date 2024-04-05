f208fbfef786e90230911f9c575f29d01336d0cd
folajhimie <blackhemisphere12@gmail.com>
2024-04-05 20:48:54 +0100

    working on the build file ...

--- src/pages/TenantDashboard/MainPage.jsx
+++ src/pages/TenantDashboard/MainPage.jsx
@@ -1,17 +1,16 @@
 
-// import Tabs from './Tabs/Index.jsx';
-// import Properties from './Properties/Index.jsx'
-import React, { lazy, Suspense } from 'react';
+import Tabs from './Tabs/Index.jsx';
+import Properties from './Properties/Index.jsx'
+// import React, { lazy, Suspense } from 'react';
 
-const Tabs = lazy(() => import('./Tabs/Index.jsx'));
-const Properties = lazy(() => import('./Properties/Index.jsx'));
+// const Tabs = lazy(() => import('./Tabs/Index.jsx'));
+// const Properties = lazy(() => import('./Properties/Index.jsx'));
 
 
 
 const MainPage = () => {
     return (
 
-        <Suspense fallback={<Spinner />}>
             <div className='h-screen text-lg font-semibold w-full z-0 relative px-6 top-[3rem]'>
 
                 <div className=''>
@@ -22,7 +21,8 @@ const MainPage = () => {
                     <Properties />
                 </div>
             </div>
-        </Suspense>
+        // <Suspense fallback={<Spinner />}>
+        // </Suspense>
     )
 }
 
--- src/pages/dashboard/MainPage.jsx
+++ src/pages/dashboard/MainPage.jsx
@@ -1,20 +1,19 @@
 
-// import Tabs from './Tabs/Index.jsx';
-// import Properties from './Properties/Index.jsx'
-// import Orders from './Orders/index.jsx';
+import Tabs from './Tabs/Index.jsx';
+import Properties from './Properties/Index.jsx'
+import Orders from './Orders/index.jsx';
 
-import React, { lazy, Suspense } from 'react';
+// import React, { lazy, Suspense } from 'react';
 
-const Tabs = lazy(() => import('./Tabs/Index.jsx'));
-const Properties = lazy(() => import('./Properties/Index.jsx'));
-const Orders = lazy(() => import('./Orders/index.jsx'));
+// const Tabs = lazy(() => import('./Tabs/Index.jsx'));
+// const Properties = lazy(() => import('./Properties/Index.jsx'));
+// const Orders = lazy(() => import('./Orders/index.jsx'));
 
-import Spinner from '../../components/Spinner/Spinner.jsx';
+// import Spinner from '../../components/Spinner/Spinner.jsx';
 
 
 const MainPage = () => {
   return (
-    <Suspense fallback={<Spinner />}>
         <div className='h-screen text-lg font-semibold w-full z-0 relative px-6 top-[3rem]'>
             
             <div className=''>
@@ -29,7 +28,7 @@ const MainPage = () => {
                 <Orders /> 
             </div>
         </div>
-    </Suspense>
+    
   )
 }
 
